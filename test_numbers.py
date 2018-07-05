dc8e84e5344e279d98ae3b312cc00fe42aa7e1cf
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-07-05 11:08:38 +0530

    unit testing now check for number of cases to be valid or not

--- test_numbers.py
+++ test_numbers.py
@@ -7,6 +7,7 @@ class RandomNumberGeneratorTesting(unittest.TestCase):
 		self.rng = RandomNumberGenerator()
 		self.rng_values = self.rng.get_values()
 
+		self.assertLess(0, self.rng_values["num_testcases"])
 		possible_print_num_testcases = ['T', 't', 'Y', 'y', 'F', 'f', 'N', 'n']
 		self.assertIn(self.rng_values['print_num_testcases'], possible_print_num_testcases)
 		self.assertIn(self.rng_values["options"]["is_float"], possible_print_num_testcases)
