67d61aaa1702bbb05303caea6d0c51243865625e
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-07-05 10:45:32 +0530

    more comments

--- numbers.py
+++ numbers.py
@@ -9,13 +9,13 @@ class RandomNumberGenerator:
 		self.num_testcases = int(stdin.readline().strip().split()[0])
 		self.values["num_testcases"] = self.num_testcases
 		
-		# whether to print number of test cases or not i.e self.num_testcases
+		# whether to print number of test cases or not i.e self.num_testcases (BOOLEAN)
 		self.print_num_testcases = stdin.readline().strip().split()[0]
 		self.values["print_num_testcases"] = self.print_num_testcases
 
 		self.options = {}
 
-		# whether the number is float or not
+		# whether the number is float or not (BOOLEAN)
 		is_float = stdin.readline().strip().split()[0]
 		self.options["is_float"] = is_float
 
