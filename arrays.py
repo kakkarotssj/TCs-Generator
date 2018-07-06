996f658f356977d598ffbc7a3147769f75f09909
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-07-06 17:34:47 +0530

    assertion statements now valid input in different validate input function rather than raising ValueError and few unnecessary loops removed

--- arrays.py
+++ arrays.py
@@ -4,6 +4,7 @@ from sys import stdin, stdout
 class RandomArrayGenerator:
 	def __init__(self):
 		self.values = {}
+		self.possible_true_false_values = ['T', 't', 'Y', 'y', 'F', 'f', 'N', 'n']
 
 		# number of test cases
 		self.num_testcases = int(stdin.readline().strip().split()[0])
@@ -37,8 +38,25 @@ class RandomArrayGenerator:
 
 		self.values["options"] = self.options
 
+		self.__validate_input()
+
 	def get_values(self):
 		return self.values
+
+	def __validate_input(self):
+		assert self.values['num_testcases'] > 0, "Number of test cases provided has to be greater than 0"
+		assert self.values['print_num_testcases'] in self.possible_true_false_values, "whether to print number of test cases or not is not understandable"
+
+		assert self.values['options']['array_size'] > 0, "Size of array has to be greater than 0"
+		assert self.values['options']['print_array_size'] in self.possible_true_false_values, "whether to print size of array or not is not understandable"
+
+		assert self.values['options']['distinct'] in self.possible_true_false_values, "whether elements in array are distinct or not is not understandable"
+		assert self.values['options']['strictly_increasing'] in self.possible_true_false_values, "whether elements in array are strictly increasing or not is not understandable"
+
+		assert self.values['options']['minvalue'] < self.values['options']['maxvalue'], "min value for array elements has to be less than max value"
+
+		if self.values['options']['distinct'] in self.possible_true_false_values[:4] or self.values['options']['strictly_increasing'] in self.possible_true_false_values[:4]:
+			assert self.values['options']['array_size'] < self.values['options']['maxvalue'] - self.values['options']['minvalue'] + 1
 
 	def _get_distinct_values(self, values, arr):
 		partitions = [[values['options']['minvalue'], values['options']['maxvalue']]]
@@ -66,29 +84,37 @@ class RandomArrayGenerator:
 		return arr
 
 	def print_values(self, values):
-		if values["print_num_testcases"] in ['T', 't', 'Y', 'y']:
+		if values["print_num_testcases"] in self.possible_true_false_values[:4]:
 			stdout.write(str(values["num_testcases"]) + '\n')
 		
+		print_array_size = False
+		if values['options']['print_array_size'] in self.possible_true_false_values[:4]:
+			print_array_size = True
+
+		distinct = False
+		if values['options']['distinct'] in self.possible_true_false_values[:4]:
+			distinct = True
+
+		strictly_increasing = False
+		if values['options']['strictly_increasing'] in self.possible_true_false_values[:4]:
+			distinct = True
+
 		for i in range(values["num_testcases"]):
-			if values["options"]["print_array_size"] in ['T', 't', 'Y', 'y']:
+			if print_array_size:
 				stdout.write(str(values["options"]["array_size"]) + '\n')
 
 			arr = []
 
 			# below are the three possible values of distinct and strictly_increasing values
 			# 1) distinct --> False and strictly_increasing --> False
-			if values["options"]["distinct"] not in ['T', 't', 'Y', 'y'] and values["options"]['strictly_increasing'] not in ['T', 't', 'Y', 'y']:
+			if not distinct and not strictly_increasing:
 				for j in range(values['options']["array_size"]):
 					arr.append(random.randint(values["options"]["minvalue"], values["options"]["maxvalue"]))
 
 			# 2) strictly_increasing --> True
-			if values["options"]["strictly_increasing"] in ['T', 't', 'Y', 'y']:
-				if values['options']['array_size'] > values["options"]["maxvalue"] - values['options']['minvalue'] + 1:
-					raise ValueError("Strictly Increasing Array can not be created because of insufficient minimum, maximum window")
-				
+			if strictly_increasing:
 				step = float(values["options"]["maxvalue"] - values["options"]["minvalue"] + 1) / float(values["options"]["array_size"])
 				if 1.0 < step < 2.0:
-					# good thing will be if edge values are included
 					if values['options']['array_size'] == 1:
 						arr.append(values['options']['minvalue'])
 					else:
@@ -106,10 +132,7 @@ class RandomArrayGenerator:
 						j += 1
 
 			# 3) distinct --> True and strictly_increasing --> False
-			if values['options']['distinct'] in ['T', 't', 'Y', 'y'] and values['options']['strictly_increasing'] not in ['T', 't', 'Y', 'y']:
-				if values['options']['array_size'] > values["options"]["maxvalue"] - values['options']['minvalue'] + 1:
-					raise ValueError(" Array can not be created because of insufficient minimum, maximum window")
-
+			if distinct and not strictly_increasing:
 				arr = self._get_distinct_values(values, arr)
 
 			# 4) distinct --> True and strictly_increasing --> True would just be same as case 2
