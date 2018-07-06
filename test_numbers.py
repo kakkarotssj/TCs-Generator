8bb3f3bea01b2cd39de6e6bf3baedd5b8c75efd9
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-07-06 15:42:25 +0530

    self.rng_values changeed to rng_values

--- test_numbers.py
+++ test_numbers.py
@@ -5,12 +5,12 @@ from numbers import RandomNumberGenerator
 class RandomNumberGeneratorTesting(unittest.TestCase):
 	def test_values(self):
 		self.rng = RandomNumberGenerator()
-		self.rng_values = self.rng.get_values()
+		rng_values = self.rng.get_values()
 
-		self.assertLess(0, self.rng_values["num_testcases"])
+		self.assertLess(0, rng_values["num_testcases"])
 		possible_print_num_testcases = ['T', 't', 'Y', 'y', 'F', 'f', 'N', 'n']
-		self.assertIn(self.rng_values['print_num_testcases'], possible_print_num_testcases)
-		self.assertIn(self.rng_values["options"]["is_float"], possible_print_num_testcases)
+		self.assertIn(rng_values['print_num_testcases'], possible_print_num_testcases)
+		self.assertIn(rng_values["options"]["is_float"], possible_print_num_testcases)
 
-		self.assertLess(self.rng_values["options"]["minvalue"], self.rng_values['options']["maxvalue"])
+		self.assertLess(rng_values["options"]["minvalue"], rng_values['options']["maxvalue"])
 		
