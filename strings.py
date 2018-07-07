0bbada352092b6dc43f5af511793b340c90e4e44
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2018-07-07 21:52:11 +0530

    validating inputs is through seperate methods now with better understanding of unit testing

--- strings.py
+++ strings.py
@@ -35,7 +35,8 @@ class RandomStringGenerator:
 		# whether uppercase, lowercase, digits, punctuation are allowed or not resp.
 		self.allowed = stdin.readline().strip().split()[0]
 		self.store_allowed(self.allowed)
-		self.possible_letters = self.get_possible_letters(self.allowed)
+
+		self.store_possible_letters(self.allowed)
 
 	def store_num_testcases(self, num_testcases):
 		if type(num_testcases) != int:
@@ -47,10 +48,13 @@ class RandomStringGenerator:
 				self.values['num_testcases'] = num_testcases
 
 	def store_print_num_testcases(self, print_num_testcases):
-		if print_num_testcases in self.possible_true_false_values:
-			self.values['print_num_testcases'] = print_num_testcases
+		if type(print_num_testcases) != str:
+			raise TypeError("whether to print num testcases or not should be string only")
 		else:
-			raise ValueError("whether to print test cases or not is invalid.")
+			if print_num_testcases in self.possible_true_false_values:
+				self.values['print_num_testcases'] = print_num_testcases
+			else:
+				raise ValueError("whether to print test cases or not is invalid.")
 
 	def store_string_size(self, string_size):
 		if type(string_size) != int:
@@ -62,32 +66,41 @@ class RandomStringGenerator:
 				self.values['options']['string_size'] = string_size
 
 	def store_print_string_size(self, print_string_size):
-		if print_string_size in self.possible_true_false_values:
-			self.values['options']['print_string_size'] = print_string_size
+		if type(print_string_size) != str:
+			raise TypeError("whether to print string size or not should be string only")
 		else:
-			raise ValueError("whether to print string size or not is invalid.")
+			if print_string_size in self.possible_true_false_values:
+				self.values['options']['print_string_size'] = print_string_size
+			else:
+				raise ValueError("whether to print string size or not is invalid.")
 
 	def store_distinct(self, distinct):
-		if distinct in self.possible_true_false_values:
-			self.values['options']['distinct'] = distinct
+		if type(distinct) != str:
+			raise TypeError("whether to have distinct characters choice should be string only")
 		else:
-			raise ValueError("whether to have distinct letters or not is invalid.")
+			if distinct in self.possible_true_false_values:
+				self.values['options']['distinct'] = distinct
+			else:
+				raise ValueError("whether to have distinct letters or not is invalid.")
 
 	def store_allowed(self, allowed):
-		if len(allowed) != 4:
-			raise ValueError("choice of allowed characters must be 4 lenghts only.")
+		if type(allowed) != str:
+			raise TypeError("Allowed characters choice must be string only.")
 		else:
-			set_allowed = list(set(allowed))
-			if len(set_allowed) == 1 and set_allowed == ['n']:
-				raise ValueError("Atleast one type of letter must be allowed.")
+			if len(allowed) != 4:
+				raise ValueError("choice of allowed characters must be 4 lenghts only.")
 			else:
-				for char in allowed:
-					if char not in self.possible_true_false_values:
-						raise ValueError("choice for allowed characters is not understandable")
+				set_allowed = list(set(allowed))
+				if len(set_allowed) == 1 and set_allowed == ['n']:
+					raise ValueError("Atleast one type of letter must be allowed.")
+				else:
+					for char in allowed:
+						if char not in self.possible_true_false_values:
+							raise ValueError("choice for allowed characters is not understandable")
 
 		self.values['options']['allowed'] = allowed
 
-	def get_possible_letters(self, allowed):
+	def store_possible_letters(self, allowed):
 		possible_letters = ''
 		for i in range(4):
 			if i == 0 and allowed[0] in self.possible_true_false_values[:4]:
@@ -102,7 +115,7 @@ class RandomStringGenerator:
 		if len(possible_letters) < self.values['options']['string_size']:
 			raise ValueError("Required amount of distinct letters cannot be created in the chosen range")
 
-		return possible_letters
+		self.values['possible_letters'] = possible_letters
 
 	def get_values(self):
 		return self.values
@@ -122,15 +135,26 @@ class RandomStringGenerator:
 		for _ in range(values['num_testcases']):
 			if print_string_size:
 				stdout.write(str(values['options']['string_size']) + '\n')
-				
-				temp_possible_letters = list(self.possible_letters)
-				j = 0
-				while j != values['options']['string_size']:
-					random_letter = temp_possible_letters.pop(random.randint(0, len(temp_possible_letters)-1))
-					ans += random_letter
-					j += 1
-				ans = ''.join(ans)
+
+			if distinct:
+				ans = get_distinct_letters(values['possible_letters'], values['options']['string_size'])
 			else:
-				ans = ''.join(random.choice(self.possible_letters) for i in range(values['options']['string_size']))
+				ans = get_random_letters(values['possible_letters'], values['options']['string_size'])
 
 			stdout.write(str(ans) + '\n')
+
+def get_distinct_letters(possible_letters, string_size):
+	temp_possible_letters = list(possible_letters)
+	j = 0
+	ans = ''
+	while j != string_size:
+		random_letter = temp_possible_letters.pop(random.randint(0, len(temp_possible_letters) - 1))
+		ans += random_letter
+		j += 1
+	
+	return ans
+
+def get_random_letters(possible_letters, string_size):
+	ans = ''.join(random.choice(possible_letters) for i in range(string_size))
+
+	return ans
